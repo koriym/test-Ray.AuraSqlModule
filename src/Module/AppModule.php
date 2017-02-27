9fec4a0a27a53691f9e5b6ff85ef93f8a13940e7
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-02-27 12:45:57 +0900

    install AuraSqlModule

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -2,7 +2,11 @@
 
 namespace MyVendor\MyProject\Module;
 
+use Aura\Sql\ConnectionLocator;
 use BEAR\Package\PackageModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+use Ray\AuraSqlModule\AuraSqlReplicationModule;
+use Ray\AuraSqlModule\Connection;
 use Ray\Di\AbstractModule;
 use josegonzalez\Dotenv\Loader as Dotenv;
 
@@ -18,5 +22,12 @@ class AppModule extends AbstractModule
             'toEnv' => true
         ]);
         $this->install(new PackageModule);
+
+        $this->install(new AuraSqlModule('mysql:host=localhost;dbname=default'));
+        $locator = new ConnectionLocator();
+        $locator->setWrite('master', new Connection('mysql:host=localhost;dbname=master', 'root', ''));
+        $locator->setRead('slave1',  new Connection('mysql:host=localhost;dbname=slave', 'root', ''));
+        $this->install(new AuraSqlReplicationModule($locator));
+        $this->install(new AuraSqlReplicationModule($locator, 'foo'));
     }
 }
